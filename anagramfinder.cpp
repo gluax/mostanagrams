ae291ee03bad1ae8fa5bd60966d483d8be129a6a
gluaxspeed <gluaxspeed@gmail.com>
2017-10-23 15:14:06 -0400

    made a non naive approach, could process multiple words at a time, and mmap the dict file but we don't expect students to do so.

--- anagramfinder.cpp
+++ anagramfinder.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <utility>
 
 using namespace std;
 
@@ -22,8 +23,6 @@ void find_anagrams(map<string, vector<string>>& anagrams, ifstream& file) {
 
     //word parsing here?
     //or do I want the original word not stripped of unwanted characters?
-    //have to go back and look at Common Word finder to see good method of doing this
-    // TODO
     for(auto c : word) {
       if(!isalpha(c)) continue;
       converted += tolower(c);
@@ -34,7 +33,6 @@ void find_anagrams(map<string, vector<string>>& anagrams, ifstream& file) {
     
     //idea #2 instead of sorting maybe assign value of primes to each letter
     //its faster to mathmatically calc a value instead of sorting a string I think
-    //but I have to convert all uppercase to lowercase as well...
     sort(sorted_word.begin(), sorted_word.end());
     
     //map the sorted letters to the vector and  push back the word to the vector
@@ -43,9 +41,30 @@ void find_anagrams(map<string, vector<string>>& anagrams, ifstream& file) {
   }
 }
 
-int main(int argc, char *argv[]) {
-  //a naive approach...
+void find_anagrams_no_sorting(map<int, vector<string>>& anagrams, ifstream& file) {
+  unsigned char primes[26] = {5,71,79,19,2,83,31,43,11,53,37,23,41,3,13,73,101,17,29,7,59,47,61,97,89,67};
+  string word;
+  unsigned int word_value = 1;
   
+  //while not end of file
+  while (true) {
+    if (file.eof()) {
+      return;
+    }
+    
+    //grab the word
+    file >> word;
+
+    for(auto c : word) {
+      word_value *= primes[tolower(c) - 'a']; 
+    }
+    
+    anagrams[word_value].push_back(word);
+    word_value = 1;
+  }
+}
+
+int main(int argc, char *argv[]) {
   //default file name
   string fname = "dictionary.txt";
 
@@ -56,8 +75,8 @@ int main(int argc, char *argv[]) {
 
   //create map of sorted characters in word to vector of the words
   //maybe map to pair of vector, # of words
-  map<string, vector<string>> anagrams;
-
+  map<int, vector<string>> anagrams;
+  
   //open the file
   //maybe mmap? Don't think there's a need to if every line only contains a single word
   ifstream file(fname);
@@ -67,7 +86,7 @@ int main(int argc, char *argv[]) {
   }
 
   //grab all the anagrams
-  find_anagrams(anagrams, file);
+  find_anagrams_no_sorting(anagrams, file);
 
   //find the largest size but then we traverse it twice...
   //once to find largest size second to see which have the largest size
